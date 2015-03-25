059c9b6e90a7d6f70011c3f70b83da0ae54d44e8
Stephen Settle <ssettle3@gmail.com>
2015-03-25 11:16:19 -0400

    gulp

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -67,6 +67,14 @@
 		}
 	])
 
+	.filter('cmdate', [
+    '$filter', function($filter) {
+        return function(input, format) {
+            return $filter('date')(new Date(input), format);
+        };
+    }
+	])
+
 	.directive('addRoom', function () {
 		return {
 			restrict: 'AC',
